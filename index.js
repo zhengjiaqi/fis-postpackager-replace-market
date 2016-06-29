d522fcb57d71bf3a6825f08a6e211d35dc07afa9
jiaqi.zheng <jiaqi.zheng@qunar.com>
2016-06-29 13:48:12 +0800

    market变量替换插件

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 module.exports = function(ret, conf, settings, opt) {
+  var inMiMarket = true;
+  try {
+    market.getDeviceInfo();
+  } catch (e) {
+    inMiMarket = false;
+  }
   fis.util.map(ret.src, function(subpath, file) {
-    if (file.isJsLike) {
+    if (file.isJsLike && !inMiMarket) {
       var content = file.getContent();
       var replaceBeforeJS =
-        'if(navigator.userAgent.toLowerCase().indexOf("mi")===-1){'+
+        //'if(navigator.userAgent.toLowerCase().indexOf("mi")===-1){'+
         '  window.market = window.marketAd = {}; '+
         '  market.registerViewStatus = marketAd.trackAdAction = window.market.registerAppStatus = window.market.checkApis = window.market.checkAppsOnMobile = function() {};'+
-        '  window.market.getDeviceInfo = function(){return "{}"}'+
-        '}'
+        '  window.market.getDeviceInfo = function(){return "{}"}'
+        //'}'
         ;
       var replaceAfterJS =
-        'if(navigator.userAgent.toLowerCase().indexOf("mi")===-1){'+
-        '  window.ajaxData = {"os":"5.11.19","model":"MI 3","ro":"unknown","marketVersion":1914102,"imei":"357911d9ab2125b3e00ecd4a6a62fc73","miuiBigVersionName":"V7-dev","resolution":"1080*1920","webResVersion":248,"clientId":"bf60126bb9e2ecdad218adaa1f075f21","densityScaleFactor":3,"co":"CN","pageConfigVersion":123,"session":"ZEtq8LRPFNXboEka","deviceType":0,"la":"zh","sdk":"19"};'+
-        '}'
+        //'if(navigator.userAgent.toLowerCase().indexOf("mi")===-1){'+
+        '  window.ajaxData = {"os":"5.11.19","model":"MI 3","ro":"unknown","marketVersion":1914102,"imei":"357911d9ab2125b3e00ecd4a6a62fc73","miuiBigVersionName":"V7-dev","resolution":"1080*1920","webResVersion":248,"clientId":"bf60126bb9e2ecdad218adaa1f075f21","densityScaleFactor":3,"co":"CN","pageConfigVersion":123,"session":"ZEtq8LRPFNXboEka","deviceType":0,"la":"zh","sdk":"19"};'
+        //'}'
         ;
       content = content.replace('/*replace-market-before*/', replaceBeforeJS);
       content = content.replace('/*replace-market-after*/', replaceAfterJS);
